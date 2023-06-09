09313800858ff1a0f84a67a30bc238ea0d2cbdaa
zhangtianlin <zhangtianlin@ebaolife.com>
2023-06-09 17:21:53 +0800

    项目初始化

--- src/main/java/com/tianlin/linpaobackend/JavaServerApplication.java
+++ src/main/java/com/tianlin/linpaobackend/JavaServerApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@MapperScan("com.tianlin.linpaobackend.mapper")
+@MapperScan("com.tianlin.linpaobackend.mapper") // 扫描 Mapper, 生成 Mapper 对象 传入的字符串要和 Mapper 的包的路径一致
 public class JavaServerApplication {
 
     public static void main(String[] args) {
