345bea0ae7c2625848b2bf7baf8cab8a413cc1aa
zhangtianlin <zhangtianlin@ebaolife.com>
2023-06-21 16:35:26 +0800

    add: 优化插入

--- src/main/java/com/tianlin/linpaobackend/once/InsertUsers.java
+++ src/main/java/com/tianlin/linpaobackend/once/InsertUsers.java
@@ -2,17 +2,21 @@ package com.tianlin.linpaobackend.once;
 
 import com.tianlin.linpaobackend.mapper.UserMapper;
 import com.tianlin.linpaobackend.model.domain.User;
+import com.tianlin.linpaobackend.service.UserService;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 import org.springframework.util.StopWatch;
 
 import javax.annotation.Resource;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 @Component // 将该类交给 Spring 管理, 变成一个 Bean
 public class InsertUsers {
 
     @Resource
-    private UserMapper userMapper;
+    private UserService userService;
 
     /**
      * 批量插入用户
@@ -21,8 +25,9 @@ public class InsertUsers {
     public void batchInsertUsers() {
         StopWatch stopWatch = new StopWatch();
         stopWatch.start();
-
         int INSERT_USER_COUNT = 1000;  //  插入用户的数量
+        // 创建用户列表
+        List<User> userList = new ArrayList<>();
         for (int i = 0; i < INSERT_USER_COUNT; i++) {
             User user = new User();
             user.setUsername("");
@@ -36,8 +41,9 @@ public class InsertUsers {
             user.setUserRole(0);
             user.setUserCode(0L);
             user.setTags("");
-            userMapper.insert(user);
+            userList.add(user);
         }
+        userService.saveBatch(userList, 100); // 批量插入用户, 每次插入 100 条
         stopWatch.stop();
         System.out.println("插入" + INSERT_USER_COUNT + "条用户数据, 耗时: " + stopWatch.getTotalTimeMillis() + "ms");
     }
