16170e435bd9fcae8502ac1085eec4fa025d36e2
zhangtianlin <zhangtianlin@ebaolife.com>
2023-07-05 17:18:57 +0800

    完成队伍页面开发

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,18 @@
-import {createApp} from 'vue'
+import {createApp, App} from 'vue'
 import './style.css'
 import 'vant/es/toast/style';
 import 'vant/es/dialog/style';
 import 'vant/es/notify/style';
 import 'vant/es/image-preview/style';
-import App from './App.vue'
+import Root from './App.vue'
 import router from './routes/index'
-import {createPinia} from 'pinia'
+import {createPinia, Pinia} from 'pinia'
 import piniaPluginPersistedstate from 'pinia-plugin-persistedstate' // 引入pinia的持久化插件
 
-const pinia = createPinia()
+const pinia: Pinia = createPinia()
 pinia.use(piniaPluginPersistedstate)
 
-const app = createApp(App)
+const app: App<Element> = createApp(Root)
 app.use(router)
 app.use(pinia)
 app.mount('#app')
